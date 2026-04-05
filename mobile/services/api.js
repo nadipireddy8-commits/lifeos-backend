45c60cf9529ce27842b81377d334143ca058b213
Jitendar  Reddy <nadipireddy8@gmail.com>
2026-04-05 14:29:35 +0530

    fix all api endpoint exports

--- mobile/services/api.js
+++ mobile/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// CORRECT live backend URL
+// Correct live backend URL
 const API_URL = 'https://lifeos-backend-017u.onrender.com/api';
 
 const api = axios.create({ baseURL: API_URL });
